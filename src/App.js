714e9fcd99965d42953897c3ab9adc63322029a2
Riham274 <riham.ararawi274@gmail.com>
2025-09-25 14:11:11 +0300

    Add sidebar, home, products pages and routing setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-
+      <Navbar />
+      <div className="row">
+        <div className="col-2 sidebar">
+          <Sidebar />
+        </div>
+        <div className="col-10">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="products" element={<Products />} />
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Sidebar.js
@@ -0,0 +1,16 @@
+function Sidebar() {
+  return (
+    <>
+      <ul className="list-unstyled">
+        <li>
+          <a href="#">get all products</a>
+        </li>
+        <li>
+          <a href="#">get all cateogries</a>
+        </li>
+      </ul>
+    </>
+  );
+}
+
+export default Sidebar;
