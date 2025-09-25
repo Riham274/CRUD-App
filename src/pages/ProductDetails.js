0276eba2c55fddfa159c70fb2837348b5f80e0b4
Riham274 <riham.ararawi274@gmail.com>
2025-09-25 15:28:59 +0300

    Create ProductDetails component to show product info

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,8 +1,17 @@
-function ProductDetails(){
-    return(
-        <>
-        </>
-    )
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function ProductDetails() {
+  let { productID } = useParams();
+  const [product, setProduct] = useState();
+  useEffect(() => {
+    fetch(`http://localhost:5000/products/${productID}`)
+      .then((res) => res.json())
+      .then((product) => {
+        setProduct(product);
+      });
+  }, []);
+  return <>{product && <h1>{product.title}</h1>}</>;
 }
 
 export default ProductDetails;
